77f9048d6c4d4ee124a9c625eda97f971d5f1a48
narendra <tammishetty15029@gmail.com>
2024-12-23 19:56:27 +0530

    configured docker compose and checks to wait for db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -6,8 +6,8 @@ import time
 from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
+
 class Command(BaseCommand):
-    
     
     """Django command to wait for database."""
     def handle(self, *args, **options):
