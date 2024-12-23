6187bdf33d5e7822fff59010e98992b1954f7286
narendra <tammishetty15029@gmail.com>
2024-12-23 20:34:13 +0530

    configured docker compose and checks to wait for db

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,7 +1,7 @@
 """Calculator functions."""
 
 
-def add(x, y):    
+def add(x, y):
     return x + y
 
 
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,10 +1,10 @@
-"""Test custom Django management commands.
-"""
+"""Test custom Django management commands."""
 
 from unittest.mock import patch
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
+
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
@@ -23,4 +23,5 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
         self.assertEqual(patched_check.call_count, 3)
         patched_check.assert_called_with(databases=['default'])
+
 
